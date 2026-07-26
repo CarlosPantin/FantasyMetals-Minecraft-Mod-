4fb2c22abd19673343bfe62aa3eb9de642800565
CarlosPantin <carlospantinpang@outlook.com>
2026-07-26 12:14:45 +0300

    Ruby pickaxe and recipe added

--- src/main/java/org/carlos/fantasymetals/item/ModItems.java
+++ src/main/java/org/carlos/fantasymetals/item/ModItems.java
@@ -3,6 +3,7 @@ package org.carlos.fantasymetals.item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,6 +17,13 @@ public class ModItems {
             ModToolMaterials.RUBY,
             new Item.Settings().attributeModifiers(
                     SwordItem.createAttributeModifiers(ModToolMaterials.RUBY, 3, -2.4F)
+            )
+    );
+
+    public static final Item RUBY_PICKAXE = new PickaxeItem(
+            ModToolMaterials.RUBY,
+            new Item.Settings().attributeModifiers(
+                    PickaxeItem.createAttributeModifiers(ModToolMaterials.RUBY, 1, -2.8F)
             )
     );
 
@@ -36,6 +44,12 @@ public class ModItems {
                 Registries.ITEM,
                 Identifier.of("fantasymetals", "ruby_ore"),
                 new BlockItem(ModBlocks.RUBY_ORE, new Item.Settings())
+        );
+
+        Registry.register(
+                Registries.ITEM,
+                Identifier.of("fantasymetals", "ruby_pickaxe"),
+                RUBY_PICKAXE
         );
     }
 }
